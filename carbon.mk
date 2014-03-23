50d61045a6781aa2d687845690d8ac73ecc125d2
rebelos <theofficiallysonyrebel@gmail.com>
2014-03-23 18:56:32 +0530

    CarbonRoms lets bring it!

--- carbon.mk
+++ carbon.mk
@@ -3,23 +3,17 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #      http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon Stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/sony/nicki/nicki.mk)
@@ -29,7 +23,7 @@ TARGET_SCREEN_WIDTH := 480
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := nicki
-PRODUCT_NAME := cm_nicki
+PRODUCT_NAME := carbon_nicki
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := C1905
 PRODUCT_MANUFACTURER := Sony
@@ -39,6 +33,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=C1905 \
     BUILD_FINGERPRINT="Sony/C1905_1274-5551/C1905:4.1.2/15.1.C.2.8/7bd96g:user/release-keys" \
     PRIVATE_BUILD_DESC="C1905-user 4.1.2 2.11.J.2.18 7bd96g test-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.nicki.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 # Release name
 PRODUCT_RELEASE_NAME := Xperiam
